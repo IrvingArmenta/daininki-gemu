5c95b9b0e06167e5b60d514cb934b8b33e6ae629
Irving Armenta <irvingarmentajd@gmail.com>
2021-05-24 22:29:56 +0900

    feat: setting up favicons, removing sound files support and updating readme and package json

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ module.exports = {
     defaultLocale: 'ja',
   },
   webpack(config, options) {
-    const { isServer } = options;
     config.module.rules.push({
       test: /\.svg$/,
       issuer: /\.(js|ts)x?$/,
@@ -18,23 +17,6 @@ module.exports = {
           titleProp: true
         }
       }, 'file-loader']
-    });
-    config.module.rules.push({
-      test: /\.(ogg|mp3|wav|mpe?g)$/i,
-      exclude: config.exclude,
-      use: [
-        {
-          loader: require.resolve('url-loader'),
-          options: {
-            limit: config.inlineImageLimit,
-            fallback: require.resolve('file-loader'),
-            publicPath: `${config.assetPrefix}/_next/static/images/`,
-            outputPath: `${isServer ? '../' : ''}static/images/`,
-            name: '[name]-[hash].[ext]',
-            esModule: config.esModule || false,
-          },
-        },
-      ],
     });
     return config;
   },
